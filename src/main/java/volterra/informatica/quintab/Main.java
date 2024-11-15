b25d6457a130484c63436f39923187cac1ad7ce0
Giovanni Chen <g.chen@donboscosandona.it>
2024-11-15 13:43:25 +0100

    Initial commit

---
+++ src/main/java/volterra/informatica/quintab/Main.java
@@ -0,0 +1,14 @@
+package volterra.informatica.quintab;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    private static final Logger log = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("Test");
+    }
+}
